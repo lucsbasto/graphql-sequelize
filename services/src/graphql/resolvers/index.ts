b446588194b9c21d4731264af270f8f2ccc4f22f
Lucas Bastos <lucasbastos@gmail.com>
2020-04-06 15:37:45 -0300

    create mutations files

--- services/src/graphql/resolvers/index.ts
+++ services/src/graphql/resolvers/index.ts
@@ -1,7 +1,10 @@
-import * as Query from './Query';
 import Chef from './Chef';
+import * as Mutation from './Mutation';
+import * as Query from './Query';
+
 const resolvers = {
   Chef,
+  Mutation,
   Query,
 };
 
--- services/src/graphql/typeDefs.ts
+++ services/src/graphql/typeDefs.ts
@@ -1,4 +1,4 @@
-import { gql } from "apollo-server";
+import { gql } from 'apollo-server';
 
 const typeDefs = gql`
   type Chef {
@@ -6,10 +6,17 @@ const typeDefs = gql`
     name: String!
     restaurants: [Restaurant!]!
   }
+
   type Restaurant {
     id: ID!
     name: String!
   }
+
+  type Mutation {
+    createChef(name: String!): Chef!
+    createRestaurant(chefId: ID!, name: String!): Restaurant!
+  }
+
   type Query {
     chefs: [Chef!]!
   }
